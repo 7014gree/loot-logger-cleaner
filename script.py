9c322ba55615e6280af0680f12120293c3780f71
7014gree <7014gree@gmail.com>
2023-03-14 19:44:32 +0000

    Added more try-except blocks to catch more errors. Added progress bar using tqdm.

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os
 import json
 from datetime import datetime, timedelta
+from tqdm import tqdm
+
 
 def make_directory(name: str):
     try:
@@ -17,22 +19,22 @@ def delete_file(name: str, log_name: str):
 def date_inputs() -> tuple([datetime, datetime]):
     start_str = input("Enter start date in format 'DD/MM/YY': ")
     end_str = input("Enter end date in format 'DD/MM/YY': ")
+
     try:
         start_date = parse_input_dates(start_str)
         end_date = parse_input_dates(end_str)  + timedelta(hours=24) - timedelta(seconds=1) # To get 23:59:59
         if end_date < start_date:
             raise ValueError(f"end date '{end_date}' is before start date '{start_date}'")
-    except ValueError as e:
-        print(f"Enter valid date strings: {str(e)}.")
-        return date_inputs()
-    except TypeError as e:
+    except (ValueError, TypeError) as e:
         print(f"Enter valid date strings: {str(e)}.")
         return date_inputs()
     
     return start_date, end_date
 
+
 def parse_input_dates(date_str: str) -> datetime:
     return datetime.strptime(date_str, '%d/%m/%y')
+
 
 def filter_data(name: str, log_name: str, start_date: datetime = datetime(2022, 1, 26, 0, 0), end_date: datetime = datetime(2022, 3, 16, 23, 59)):
     # Ensures file is overwritten by just deleting it
@@ -40,12 +42,19 @@ def filter_data(name: str, log_name: str, start_date: datetime = datetime(2022, 
     delete_file(f'{name}-non', log_name)
 
     with open(f'data/{name}/{log_name}', 'r') as read_file, open(f'data/{name}-leagues/{log_name}', 'a') as leagues_file, open(f'data/{name}-non-leagues/{log_name}', 'a') as non_leagues_file:
-        for line in read_file:
-            json_data = json.loads(line)
-            if start_date < datetime.strptime(json_data['date'], '%b %d, %Y, %H:%M:%S %p') < end_date:
-                leagues_file.write(line) # Automatically adds line break for append mode
-            else:
-                non_leagues_file.write(line)
+        for i, line in enumerate(read_file):
+            try:
+                json_data = json.loads(line)
+                try:
+                    if start_date < datetime.strptime(json_data['date'], '%b %d, %Y, %H:%M:%S %p') < end_date:
+                        leagues_file.write(line) # Automatically adds line break for append mode
+                    else:
+                        non_leagues_file.write(line)
+                except (ValueError, TypeError):
+                    print(f"\nError loading line {i} from '{log_name}': date in unexpected format. Data excluded from output.")
+            except json.JSONDecodeError:
+                print(f"\nError loading line {i} from '{log_name}': data not in valid JSON format. Data excluded from output.")
+
 
 if __name__ == "__main__":
     while True:
@@ -68,7 +77,7 @@ if __name__ == "__main__":
         else:
             print('Default dates used.')
 
-        for log_name in log_file_paths:
+        for log_name in tqdm(log_file_paths, total=len(log_file_paths)):
             filter_data(**kwargs, log_name=log_name)
 
         if input(f"Filter complete for /data/{name}.\nEnter 'Y' to filter another folder: ").upper() != "Y":
