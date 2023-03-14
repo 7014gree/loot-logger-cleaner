ed615951be3e06a8c04258e084b290357525e696
7014gree <7014gree@gmail.com>
2023-03-14 22:00:51 +0000

    Added script to retrieve data from s3 bucket. Added current file string to progress bar. Refactored code a bit (moved all code for taking date inputs into date_inputs, made iterating through log files it's own function as it became cluttered with progress bar

--- script_s3.py
+++ script_s3.py
@@ -2,7 +2,9 @@ import os
 import json
 from datetime import datetime, timedelta
 from tqdm import tqdm
-from time import sleep
+import boto3
+from io import TextIOWrapper
+
 
 def make_directory(name: str):
     try:
@@ -36,13 +38,14 @@ def date_inputs(filter_kwargs) -> dict:
 def parse_input_dates(date_str: str) -> datetime:
     return datetime.strptime(date_str, '%d/%m/%y')
 
-def filter_data(name: str, log_name: str, start_date: datetime = datetime(2022, 1, 26, 0, 0), end_date: datetime = datetime(2022, 3, 16, 23, 59)):
-    # Ensures file is overwritten by just deleting it
-    delete_file(name, log_name)
-    delete_file(f'{name}-non', log_name)
+def filter_data_s3(s3_client, bucket_name: str, name: str, timestamp: 'str', log_name: str, start_date: datetime = datetime(2022, 1, 26, 0, 0), end_date: datetime = datetime(2022, 3, 16, 23, 59)):
+    log_file_obj = s3_client.get_object(Bucket=bucket_name, Key=log_name) # Get object from s3 bucket
+    log_file_contents = TextIOWrapper(log_file_obj['Body']) # Retrieve text in a format that can be read line by line
+    log_name_str = log_name[len(name):] # Removes 'name/' from start of file name
 
-    with open(f'data/{name}/{log_name}', 'r') as read_file, open(f'data/{name}-leagues/{log_name}', 'a') as leagues_file, open(f'data/{name}-non-leagues/{log_name}', 'a') as non_leagues_file:
-        for i, line in enumerate(read_file):
+    with open(f'data/{name}-{timestamp}-leagues/{log_name_str}', 'a') as leagues_file, open(f'data/{name}-{timestamp}-non-leagues/{log_name_str}', 'a') as non_leagues_file:
+        for i, line in enumerate(log_file_contents):
+            line = f'{line.strip()}\n' # For some reason there was an extra line break when reading the file, fixed by removing line breaks then adding one back
             try:
                 json_data = json.loads(line)
                 try:
@@ -58,28 +61,35 @@ def filter_data(name: str, log_name: str, start_date: datetime = datetime(2022, 
 def iterate_log_files(log_file_paths: list, filter_kwargs: dict):
     progress_bar = tqdm(log_file_paths, total=len(log_file_paths))
     for log_name in progress_bar:
-        progress_bar.set_description(f"Current file: {log_name}")
-        filter_data(**filter_kwargs, log_name=log_name)
-        sleep(0.1) # File runs too quick to see progress bar
-
+        progress_bar.set_description(f"Current file: {log_name[len(name) + 1:]}")
+        filter_data_s3(**filter_kwargs, log_name=log_name)
 
 if __name__ == "__main__":
+
+    bucket_name = 'loot-logger-data-ea2cbe2a-7ec4-40ae-9480-2423d82c28ca'
+    s3_client = boto3.client('s3')
+
+    response = s3_client.list_objects_v2(Bucket=bucket_name,Delimiter='/')
+    folder_names = [folder['Prefix'][:-1] for folder in response['CommonPrefixes']]
+
     while True:
-        print(os.listdir('data'))
+        print(f"Folder names: {folder_names}")
         name = input("Enter a folder name to filter: ")
-        while os.path.exists(f'data/{name}') == False:
+        while name not in folder_names:
             name = input("Enter a folder name that exists: ")
         
-        make_directory(name)
-        make_directory(f'{name}-non')
+        timestamp = datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+        make_directory(f"{name}-{timestamp}")
+        make_directory(f"{name}-{timestamp}-non")
 
-        log_file_paths = os.listdir(f'data/{name}')
+        log_file_paths = [object['Key'] for object in s3_client.list_objects_v2(Bucket=bucket_name,Prefix=f'{name}/')['Contents']]
         
-        # If user inputs dates, returns dictionary including dates. Otherwise returns dictionary only containing name.
-        filter_kwargs = date_inputs({'name': name})
+        filter_kwargs = {'name': name, 'timestamp': timestamp, 's3_client': s3_client, 'bucket_name': bucket_name}
+
+        filter_kwargs = date_inputs(filter_kwargs)
 
         iterate_log_files(log_file_paths, filter_kwargs)
         
-        if input(f"Filter complete for /data/{name}.\nEnter 'Y' to filter another folder: ").upper() != "Y":
+        if input(f"Filter complete for {name}-{timestamp}.\nEnter 'Y' to filter another folder: ").upper() != "Y":
             break
     
